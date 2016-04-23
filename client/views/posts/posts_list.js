14a9103fc8e6feb9bfdca039b79be0b39c6f4996
Tiago Silva Pereira <tiagosilvapereira3@gmail.com>
2016-04-23 11:44:58 -0300

    Adicionando coleções do banco e removendo o autopublish

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,22 +1,6 @@
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        author: 'Sacha Greif',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    }, 
-    {
-        title: 'Meteor',
-        author: 'Tom Coleman',
-        url: 'http://meteor.com'
-    }, 
-    {
-        title: 'The Meteor Book',
-        author: 'Tom Coleman',
-        url: 'http://themeteorbook.com'
-    }
-];
-
 // Adiciono um helper para o template postsList
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
